d4985f9ef6881b1136f9a0b8bb2fa7ab7107508e
Daniil/Pecherin <daniel.pechyorin@mail.ru>
2021-12-21 00:06:08 +0300

    next commit

--- books/views.py
+++ books/views.py
@@ -1,7 +1,14 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
+
+from books.models import Book
+
 
 
 def books_view(request):
     template = 'books/books_list.html'
-    context = {}
+    books = Book.objects.all()
+    context = {
+        "books": books
+    }
     return render(request, template, context)
